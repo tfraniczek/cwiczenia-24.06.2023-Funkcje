937bd33487129cfbb8aa9b0160e1ac5a030a7e7f
tfraniczek <tymoteusz.franiczek@gmail.com>
2023-06-24 12:49:38 +0200

    2

---
+++ cw1_formatowanie_czasu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw1_formatowanie_czasu
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę całkowitą: ");
+            int sekundy = Convert.ToInt32(Console.ReadLine());
+            zmianaNaCzas(sekundy);
+        }
+
+        static void zmianaNaCzas(int sekundy)
+        {
+            TimeSpan dodaj_sekundy = TimeSpan.FromSeconds(sekundy);
+            TimeSpan czasStartowy = new TimeSpan (0, 0, 0);
+            TimeSpan wynik = czasStartowy + dodaj_sekundy;
+            Console.WriteLine(wynik.ToString());
+        }
+    }
+}
